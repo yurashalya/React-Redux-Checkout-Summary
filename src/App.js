408cc6f249013097e9339ab726b5b3090e7f0973
yurashalya <stallker9696@gmail.com>
2018-09-10 00:07:08 +0300

    Create new component Total and PickupSave

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import { Grid } from 'react-bootstrap';
+import SubTotal from './components/Subtotal/Subtotal';
+import PickupSavings from './components/Subtotal/PickupSavings/PickupSavings';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 120,
+      PickupSavings: -4.15
+    };
+  }
+
   render() {
     return (
       <div className="container"> 
         <Grid className="purchase-card">
-          <h1>Hello Yura</h1>
+          <SubTotal price={this.state.total.toFixed(2)} />
+          <PickupSavings price={this.state.PickupSavings} />
         </Grid>
       </div>
     );
---
+++ src/components/Subtotal/PickupSavings/PickupSavings.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Row, Col, Tooltip, OverlayTrigger } from 'react-bootstrap';
+
+var styles = {
+	pickupSavings: {
+		textDecoration: 'underline'
+	},
+	totalSavings: {
+		color: '#23ede9',
+		fontWeight: 800
+	}
+}
+
+export default class PickupSavings extends Component {
+	render() {
+		const tooltip = (
+			<Tooltip id ="tooltip">
+				<p>
+					Подбор вашего заказа в магазине помогает сократить расходы и мы передаем вам скидку.
+				</p>
+			</Tooltip>
+		);
+
+		return(
+			<Row className="show-grid"> 
+				<Col md={6}>
+					<OverlayTrigger placement="bottom" overlay={tooltip}> 
+						<div style={styles.pickupSavings}>Подобрать сохранения</div>
+					</OverlayTrigger>
+				</Col>
+				<Col style={styles.totalSavings} md={6}> 
+					{`$${this.props.price}`}
+				 </Col>		
+			</Row>
+		)
+	}
+}
