284e49750eb5463fd7496988dbe6bfab6a48d78a
yurashalya <stallker9696@gmail.com>
2018-09-09 23:06:23 +0300

    Connected react-bootstrap / create new css style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Grid } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div> 
-        <h1>Hello World</h1>
+      <div className="container"> 
+        <Grid className="purchase-card">
+          <h1>Hello Yura</h1>
+        </Grid>
       </div>
     );
   }
