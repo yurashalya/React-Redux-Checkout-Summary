77ac85e7706d3fcb888d64d2f01782039b419314
yurashalya <stallker9696@gmail.com>
2018-09-10 00:20:30 +0300

    New component Taxes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Grid } from 'react-bootstrap';
 import SubTotal from './components/Subtotal/Subtotal';
-import PickupSavings from './components/Subtotal/PickupSavings/PickupSavings';
+import PickupSavings from './components/PickupSavings/PickupSavings';
+import Taxes from './components/Taxes/Taxes';
 import './App.css';
 
 class App extends Component {
@@ -10,7 +11,8 @@ class App extends Component {
 
     this.state = {
       total: 120,
-      PickupSavings: -4.15
+      PickupSavings: -4.15,
+      taxes: 0
     };
   }
 
@@ -20,6 +22,8 @@ class App extends Component {
         <Grid className="purchase-card">
           <SubTotal price={this.state.total.toFixed(2)} />
           <PickupSavings price={this.state.PickupSavings} />
+          <Taxes taxes= {this.state.taxes.toFixed(2)} />
+          <hr />
         </Grid>
       </div>
     );
